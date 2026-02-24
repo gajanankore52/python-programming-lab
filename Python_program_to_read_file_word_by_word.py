a0ae09016019aca977b3c27012f3e6b6b04e2b9e
Python_Demo <gajanankore52@gmail.com>
2026-02-24 23:04:53 +0530

    Initial Code

---
+++ Python_program_to_read_file_word_by_word.py
@@ -0,0 +1,16 @@
+# Python program to read file word by word
+
+
+def read_word_by_word(file_path):
+    try:
+        with open(file_path,'r',encoding='utf-8') as file:
+            for line in file:
+                # .split() handles spaces,tabs, and newlines
+                words = line.split()
+                for word in words:
+                    print(word)
+    except FileNotFoundError:
+        print('File not found.')
+       
+# Usage
+read_word_by_word('original.txt')
