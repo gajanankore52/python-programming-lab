bd5c386e75c3037f80ab84d5af8d4f5fff396b54
Python_Demo <gajanankore52@gmail.com>
2026-03-10 19:56:29 +0530

    Initial Code

---
+++ Find_Repeated_Items_in_Tuple.py
@@ -0,0 +1,24 @@
+# Write a Python program to find repeated items in a tuple.
+
+# A tuple with repeated elements
+
+my_tuple = (10, 20, 30, 20, 40, 50, 30, 30, 60, 10)
+
+# def find_repeats(tup):
+    
+    # seen = set()
+    # repeats = set()
+    
+    # for item in tup:
+        # if item in seen:
+            # repeats.add(item)
+        # else:
+            # seen.add(item)
+            
+    # return list(repeats)
+
+
+# result = find_repeats(my_tuple)
+# print(f"Repeated items: {result}")
+
+
