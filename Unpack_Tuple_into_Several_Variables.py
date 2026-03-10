fb351f623cf8cbb3f9bbd362cd819493a1d3a38c
Python_Demo <gajanankore52@gmail.com>
2026-03-10 19:41:20 +0530

    Initial Code

---
+++ Unpack_Tuple_into_Several_Variables.py
@@ -0,0 +1,13 @@
+# Write a Python program to unpack a tuple into several variables.
+
+# Define a tuple with three elements
+
+coordinates = (10, 20, 30)
+
+# Unpack the tuple into three separate variable
+
+x, y, z = coordinates
+
+print(f"X: {x}, Y: {y}, Z: {z}")
+
+# Output: X: 10, Y: 20, Z: 30
