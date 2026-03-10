fcc0be6b9f2fbe5a38832cd89f5eeaf1265c54bd
Python_Demo <gajanankore52@gmail.com>
2026-03-10 19:49:23 +0530

    Initial Code

---
+++ Convert_Tuple_to_String.py
@@ -0,0 +1,10 @@
+# Write a Python program to convert a tuple to a string.
+
+
+# A tuple of strings
+words = ("Python", "is", "a", "versatile", "language")
+
+# Using .join() with a space as the separator
+result_string = " ".join(words)
+
+print(f"Result: {result_string}")
