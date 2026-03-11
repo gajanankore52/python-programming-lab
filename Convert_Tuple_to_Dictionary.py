ce0b31ce991e5c95ae42145825cff0ddc9ca7e6d
Python_Demo <gajanankore52@gmail.com>
2026-03-11 19:50:17 +0530

    Initial Code

---
+++ Convert_Tuple_to_Dictionary.py
@@ -0,0 +1,10 @@
+# Write a Python program to convert a tuple to a dictionary.
+
+# A tuple containing key-value pairs
+
+tuple_of_pairs = (("name", "Alice"), ("age", 30), ("city", "New York"))
+
+# Direct conversion
+result_dict = dict(tuple_of_pairs)
+
+print(result_dict)
