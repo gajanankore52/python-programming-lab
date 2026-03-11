f332518c5d069902846f3a602637e30a1799dcd8
Python_Demo <gajanankore52@gmail.com>
2026-03-11 20:05:27 +0530

    Update Code

--- Remove_Empty_Tuple_from_List_of_Tuples.py
+++ Remove_Empty_Tuple_from_List_of_Tuples.py
@@ -10,3 +10,22 @@ l = [(1, 2), (), (3, 4), (), ((),), (8, 9), ()]
 result = [t for t in l if t]
 
 print(result)
+print('*'* 30)
+
+l = [(1, 2), (), (3, 4), (), (8, 9)]
+
+# filter(None, sequence) removes all Falsy values
+result = list(filter(None, l))
+
+print(result)
+
+print('*'* 30)
+
+
+l = [(1, 2), (), ((),)]
+
+# Removes () and any tuple that is empty after being stripped of whitespace/nulls
+# Here we check if the length of the tuple is greater than 0
+result = [t for t in l if len(t) > 0]
+
+print(result) 
