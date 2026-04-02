d8e3bdf042f386de33e361523c0c827bad9eca88
Python_Demo <gajanankore52@gmail.com>
2026-04-02 13:55:53 +0530

    feat: implement selective string-to-int conversion in nested tuples using lambda

--- 33_tuple_data_conversion.py
+++ 33_tuple_data_conversion.py
@@ -3,6 +3,7 @@
 
 tuple_str =  (('233', 'ABCD', '33'), ('1416', 'EFGH', '55'), ('2345', 'WERT', '34'))
 
-result = tuple(map(lambda x:(int(x[0]),int(x[2])),tuple_str))
+# Unpacking (id, label, value) makes the logic self-documenting
+result = tuple(map(lambda item: (int(item[0]), int(item[2])), tuple_str))
 
 print(result)
