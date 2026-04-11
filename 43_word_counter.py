9bdac8b47c7c71fd7bebc58a5e542dacbd849e91
Python_Demo <gajanankore52@gmail.com>
2026-04-11 23:03:00 +0530

    feat: add word count utility using regex for punctuation handling

--- 43_word_counter.py
+++ 43_word_counter.py
@@ -1,22 +1,19 @@
-# Python File I/O: Takes a text file as input and returns the number of words of a given text file
 
 # Write a Python program that takes a text file as input and returns the number of words of a given text file.
 
 import re
 
 def count_words_in_file(file_path):
-    
+    word_count = 0
     try:
+        # 'utf-8' is the standard encoding for text files
         with open(file_path,'r',encoding='utf') as file:
+            for line in file:
+                # Find all owrds in the current line and add to total
+                words = re.findall(r'\w+',line)
+                word_count += len(words)
             
-            content = file.read()
-            
-            # regex \w+ matches one or more alphanumeric characters
-            # This treats "word1,word2" as two separate words
-            
-            words = re.findall(r'\w+',content)
-            
-            return len(words)
+        return word_count
             
     except FileNotFoundError:
         return "Error: The file was not found."
